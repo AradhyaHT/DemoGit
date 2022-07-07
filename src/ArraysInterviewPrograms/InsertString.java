71e4b5ddba9f48e3b5501833c53ba1b1db7978fb
Aradhya <dhyaa0427@gmail.com>
2022-07-07 16:04:56 +0530

    Text file and Insert String class some code is added

--- src/ArraysInterviewPrograms/InsertString.java
+++ src/ArraysInterviewPrograms/InsertString.java
@@ -7,6 +7,9 @@ public class InsertString {
 		
 		System.out.println(" Project Jira1 ");
 		System.out.println("Project Jira2 ");
+		System.out.println(" Project jira3 ");
+		System.out.println("Project jira4");
+		System.out.println("Project jira5");
 		
 	}
 }
